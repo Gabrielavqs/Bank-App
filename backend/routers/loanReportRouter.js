cc96389ffa09c46d4adb36e49f4966dff053a17f
Gabriela <gabrielavanessa.designer@gmail.com>
2025-06-06 17:21:04 -0300

    feat(reportRouter): add authenticated routes for fetching and creating loan reports

---
+++ backend/routers/loanReportRouter.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import {
+    getReportData, 
+    createReportData
+} from '../controllers/loanReportController.js';
+import authMiddleware from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.get('/', authMiddleware, getReportData);
+router.post('/',authMiddleware, createReportData);
