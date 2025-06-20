153234aa2f17060d5e327643adde8bf64c07d75c
Gabriela <gabrielavanessa.designer@gmail.com>
2025-06-20 18:29:36 -0300

    Add userRouter.js with auth-protected user routes

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,10 +1,16 @@
 import {Router} from "express";
 import {
     registerUser,
-    loginUser
+    loginUser,
+    updateUserProfile,
+    getUserProfile
 } from "../controllers/userController.js";
+import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = Router();
+
+router.get('/:id/profile', authMiddleware, getUserProfile);
+router.put('/:id/update', authMiddleware, updateUserProfile);
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
